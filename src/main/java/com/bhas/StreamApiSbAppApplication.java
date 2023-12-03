ca7489fa94d13c594385f6b3bf37b0a88cbe37c4
Naveen <naveenkrishnawodeyar@gmail.com>
2023-12-03 23:11:26 +0530

    initial commit

---
+++ src/main/java/com/bhas/StreamApiSbAppApplication.java
@@ -0,0 +1,15 @@
+package com.bhas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamApiSbAppApplication 
+{
+
+	public static void main(String[] args) 
+	{
+		SpringApplication.run(StreamApiSbAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bhas/StreamApiSbAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bhas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreamApiSbAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
