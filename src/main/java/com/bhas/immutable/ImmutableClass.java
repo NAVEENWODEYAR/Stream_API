71fa51b4565e9145dbecd222f31c8befaa0b3f38
Naveen <naveenkrishnawodeyar@gmail.com>
2023-12-05 19:14:05 +0530

    rules to achieve immutability in java class

--- src/main/java/com/bhas/immutable/ImmutableClass.java
+++ src/main/java/com/bhas/immutable/ImmutableClass.java
@@ -1,13 +1,40 @@
 package com.bhas.immutable;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+// make the class final, doesn't allows to extend or subClassed,
 public final class ImmutableClass 
 {
+	// Encapsulate the state of the object by making fields private and final. 
 	private final int clsId;
 	private final String clsName;
+	private final List<String> classPropertis;
 	
-	private ImmutableClass(int id,String name)
+	// Ensure no mutable objects are exposed:
+	public ImmutableClass(int id,String name,List<String> list)
 	{
 		this.clsName = name;
 		this.clsId = id;
+		this.classPropertis = new ArrayList<>(list);
 	}
+	
+	// Provide only getters, no setters,
+	public int getId()
+	{
+		return this.clsId;
+	}
+	
+	public String getName()
+	{
+		return this.clsName;
+	}
+	
+	public List<String> getProperties()
+	{
+		return Collections.unmodifiableList(classPropertis);
+	}
+	
+
 }
