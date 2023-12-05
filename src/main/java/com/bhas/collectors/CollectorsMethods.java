dd18060b54ff09ecc4ac1db88ac779d98ddf25b5
Naveen <naveenkrishnawodeyar@gmail.com>
2023-12-05 19:36:32 +0530

    Collectors.toSet()

--- src/main/java/com/bhas/collectors/CollectorsMethods.java
+++ src/main/java/com/bhas/collectors/CollectorsMethods.java
@@ -2,6 +2,7 @@ package com.bhas.collectors;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 public class CollectorsMethods 
@@ -21,13 +22,22 @@ public class CollectorsMethods
 	
 	public static void main(String[] args) 
 	{
-		List<Integer> oddNumbers = Arrays.asList(1,3,5,7,9);
+		List<Integer> oddNumbers = Arrays.asList(1,3,5,7,9,1,3,5,7);
 		System.out.println("*********");
-		// 1. Creating list: toList(),
+		/*
+		// 1. Creating list: toList(),accumulates elements into a list
 				System.out.println("\n Collectors.toList()");
 				List<Integer> powerOfNumber = oddNumbers.stream().map(n -> n*n).collect(Collectors.toList());
 				System.out.println("Original list,"+oddNumbers);
 				System.out.println("Modified list,"+powerOfNumber);
+				
+		*/
+		// 2. Creating set: toSet(), accumulates elements into a set(unique values)
+				System.out.println("\n Collectors.toset()");
+				Set<Integer> uniqueNumbers = oddNumbers.parallelStream().collect(Collectors.toSet());
+				System.out.println("List priovided,"+oddNumbers);
+				System.out.println("Set obtained,"+uniqueNumbers);
+				
 	}
 	
 
