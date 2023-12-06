46c89919466592574a5401ff3373f74b2287cd34
Naveen <naveenkrishnawodeyar@gmail.com>
2023-12-06 16:49:02 +0530

    Collectors.summingInt(n->n)

--- src/main/java/com/bhas/collectors/CollectorsMethods2.java
+++ src/main/java/com/bhas/collectors/CollectorsMethods2.java
@@ -17,7 +17,7 @@ public class CollectorsMethods2
 		List<Integer> oddNumbers = Arrays.asList(1,3,5,7,9,1,3,5,7);
 		System.out.println("*********");
 		
-		/* */
+		/* 
 		// 1. Averaging long: averagingLong(),Finds the average value of a collection of long values,  It will return a Double value, not a long value.
 			System.out.println("\n Collectors.averagingLong(n->n*n) ");
 			byte avgValue = oddNumbers.parallelStream().collect(Collectors.averagingLong(n->n*n)).byteValue();
@@ -27,5 +27,11 @@ public class CollectorsMethods2
 			System.out.println("\n Collectors.averagingLong(n->n*n) ");
 			int avgInt = oddNumbers.stream().collect(Collectors.averagingInt(n->n+1)).intValue();
 			System.out.println("Average value in the form of int,"+avgInt);
+	
+		*/
+	    // 3. Summing Integers: summingInt(),
+			System.out.println("\n Collectors.averagingLong(n->n*n) ");
+			int sum = oddNumbers.stream().collect(Collectors.summingInt(n->n)).intValue();
+			System.out.println("Sum of the collection,"+sum);
 	}
 }
