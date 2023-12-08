ac4af91b986c52e1835398c01d8bc7e214e2ea3c
Naveen <naveenkrishnawodeyar@gmail.com>
2023-12-08 22:33:44 +0530

    Employee.java(POJO Class)

--- src/main/java/com/bhas/comparator/ComparatorEx.java
+++ src/main/java/com/bhas/comparator/ComparatorEx.java
@@ -1,11 +1,26 @@
 package com.bhas.comparator;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class ComparatorEx 
 {
 
 	public static void main(String[] args) 
 	{
-
+		// arrayList to store values,
+		List<Employee> empList = new ArrayList<>();
+		
+						empList.add(new Employee("Bhaskar K", 25, 95000.00));
+						empList.add(new Employee("Nandi", 3, 5000.00));
+						empList.add(new Employee("Gowri", 5, 85000.00));
+						empList.add(new Employee("Gani", 6, 75000.00));
+						
+		// getMaxSalary,
+						Employee maxSal = empList.parallelStream().collect(Collectors.maxBy(Comparator.comparing(Employee::getEmpSalary))).get();
+						System.out.println("Maximum salary in the list,"+maxSal.getEmpSalary()+"-"+maxSal.getEmpName());
 	}
 
 }
