e5a613236e22cf9af5e12bc39d8ee7a821fc7bdf
Naveen <naveenkrishnawodeyar@gmail.com>
2023-12-11 02:36:51 +0530

    logOut

--- src/main/java/com/bhas/strmIntro/StreamExamples2.java
+++ src/main/java/com/bhas/strmIntro/StreamExamples2.java
@@ -9,8 +9,15 @@ public class StreamExamples2 {
 	public static void main(String[] args) {
 		System.out.println("\n ******** \n");
 		
-		//1. Finding And Matching Operations,
-		boolean anyMatch = Arrays.stream(new String[] {"Bhaskar","Nandi","Gowri","Gani","Lakshmi","MahaLakshmi"}).anyMatch(s->s.startsWith("g"));
-		System.out.println("Found the matched string,"+anyMatch);
+		//1. Finding And Matching Operations,anyMatch() : Any one element matches
+		boolean anyMatch = Arrays.stream(new String[] {"Bhaskar","Nandi","Gowri","Gani","Lakshmi","MahaLakshmi"}).anyMatch(s->s.startsWith("G"));
+		System.out.println("\nFound the matched string,"+anyMatch);
+		
+		//2. allMatch() : All elements matches,
+		boolean allMatch = Arrays.stream(new int[] {3,5,7,9}).allMatch(n->n/2!=0);
+		System.out.println("\nGiven list is odd,"+allMatch);
+		
+		boolean noneMatch = Arrays.stream(new int[] {1,2,3,4,5,6,7,8}).noneMatch(n->n*n >1);
+		System.out.println(noneMatch);
 	}
 }
