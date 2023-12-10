3a407e503c5fd1a9125c592802bbec9f97efc985
Naveen <naveenkrishnawodeyar@gmail.com>
2023-12-10 21:35:42 +0530

    StreaAPI

---
+++ src/main/java/com/bhas/strmIntro/StreamExamples.java
@@ -0,0 +1,15 @@
+package com.bhas.strmIntro;
+
+public class StreamExamples 
+{
+	static
+	{
+		System.out.println("\n Streams can be defined as a sequences of elements from a source which support data processing operations. "
+				+ "\n You can treat streams as operations on data.");
+	}
+	
+	public static void main(String[] args) 
+	{
+		System.out.println("\n ******** \n");
+	}
+}
