db5a7a5a01d5d62396c9f96c867f4ffda6d2bcf7
Naveen <naveenkrishnawodeyar@gmail.com>
2023-12-11 20:16:25 +0530

    static method ref

---
+++ src/main/java/com/bhas/methdRef/MethodReferencing.java
@@ -0,0 +1,17 @@
+package com.bhas.methdRef;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class MethodReferencing {
+
+	public static void main(String[] args) {
+		
+		System.out.println("\n shortened versions of lambda expressions calling a specific method");
+
+		// a) Method reference to static method,
+		String string = Arrays.asList("One","Two","Three","Four","Five").stream().max(Comparator.comparing(String::length)).get();
+		System.out.println("\n String with the max length,"+string+" of length "+string.length());
+	}
+
+}
