0a02e131a72db8f8e0d54e33369ff458592ecbc6
Naveen <naveenkrishnawodeyar@gmail.com>
2023-12-12 22:34:10 +0530

    stream operations

---
+++ src/main/java/com/bhas/strmOperations/IntermediateOperations.java
@@ -0,0 +1,16 @@
+package com.bhas.strmOperations;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class IntermediateOperations {
+	{
+		System.out.println("\nAs intermediate operations return another stream as a result, they can be chained together to form a pipeline of operations. "
+				+ "Terminal operations can not be chained together.");
+	}
+	public static void main(String[] args) {
+		System.out.println("\n Intermediate operations,");
+		List<String> list = Arrays.asList("Ammie","Bella","Catherine","Dora","Emmie","Florence");
+	}
+
+}
