bba30cfefffd83189727be5839f777e904b4705d
Naveen <naveenkrishnawodeyar@gmail.com>
2023-12-13 18:18:33 +0530

    intern()

--- src/main/java/com/bhas/scp/StringCompare.java
+++ src/main/java/com/bhas/scp/StringCompare.java
@@ -3,6 +3,20 @@ package com.bhas.scp;
 public class StringCompare 
 {
 
+	static void internString()
+	{
+		String s = "One";
+		String s1 = "Two";
+		String s2 = "Three";
+		String s3 = "Four";
+		String s4 = new String("Four");
+		String s5 = s4;
+		
+		//  intern() creates an exact copy of a String object in the heap memory and stores it in the String constant pool.
+		System.out.println("\n**str.intern()***");
+		System.out.println(s5.intern());
+	}
+	
 	public static void main(String[] args) 
 	{
 		String st = "String pool is nothing but a storage area in Java heap where string literals stores";
@@ -15,6 +29,8 @@ public class StringCompare
 			System.out.println("\nequals() method for content comparison.");
 		else
 			System.out.println("\n*****");
+		
+		internString();
 	}
 
 }
