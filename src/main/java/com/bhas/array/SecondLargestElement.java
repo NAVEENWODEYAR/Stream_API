4c5dce43da7bfacdfb3f8e8cc11056e8e659e741
Naveen <naveenkrishnawodeyar@gmail.com>
2023-12-13 19:05:43 +0530

    Exception in thread main java.util.NoSuchElementException: No value present

--- src/main/java/com/bhas/array/SecondLargestElement.java
+++ src/main/java/com/bhas/array/SecondLargestElement.java
@@ -1,6 +1,10 @@
 package com.bhas.array;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.NoSuchElementException;
 
 public class SecondLargestElement 
 {
@@ -12,9 +16,16 @@ public class SecondLargestElement
 	
 	public static void main(String[] args) 
 	{
-		System.out.println("\n Homogeneous collectin of elements stored at contiguous memory locations");
+		System.out.println("\n Homogeneous collection of elements stored at contiguous memory locations");
 		int[] nums = {5,7,3,1,8,0};
 		secLargestInArray(nums);
+		
+		Arrays.stream(nums).distinct().sorted().skip(1).findFirst().ifPresent(System.out::println);
+		List<Integer> list = new ArrayList(Arrays.asList(nums));
+//		list.stream().distinct().sorted(Comparator.reverseOrder()).skip(1).findFirst().orElseThrow(()-> new NoSuchElementException("No value"));
+		
+		Integer secL = list.stream().distinct().sorted(Comparator.reverseOrder()).skip(1).findFirst().get();
+//		System.out.println(list);
 	}
 
 }
