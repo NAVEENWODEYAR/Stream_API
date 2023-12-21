7f5f7e341fc71cdf1eaf863d2251b9cfb7044843
Naveen <naveenkrishnawodeyar@gmail.com>
2023-12-21 07:54:32 +0530

    GM

---
+++ src/main/java/com/bhas/date/DateFormates.java
@@ -0,0 +1,12 @@
+package com.bhas.date;
+
+import java.util.Date;
+
+public class DateFormates {
+
+	public static void main(String[] args) {
+		Date date = new Date(System.currentTimeMillis());
+		System.out.println(date);
+	}
+
+}
