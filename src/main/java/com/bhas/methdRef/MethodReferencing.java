7dc951745f82926628b67057177026f25357f32e
Naveen <naveenkrishnawodeyar@gmail.com>
2023-12-21 07:57:37 +0530

    code format

--- src/main/java/com/bhas/methdRef/MethodReferencing.java
+++ src/main/java/com/bhas/methdRef/MethodReferencing.java
@@ -10,7 +10,10 @@ public class MethodReferencing {
 		System.out.println("\n shortened versions of lambda expressions calling a specific method");
 
 		// a) Method reference to static method,
-		String string = Arrays.asList("One","Two","Three","Four","Five").stream().max(Comparator.comparing(String::length)).get();
+		String string = Arrays.asList("One","Two","Three","Four","Five")
+								.stream()
+								.max(Comparator.comparing(String::length))
+								.get();
 		System.out.println("\n String with the max length,"+string+" of length "+string.length());
 
 	}
