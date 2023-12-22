be5b16e875ee21dd79fe92bbfa9e2de665254aa0
Naveen <naveenkrishnawodeyar@gmail.com>
2023-12-22 23:55:45 +0530

    LocalDate.now()

--- src/main/java/com/bhas/lambda/LambdaImpl2.java
+++ src/main/java/com/bhas/lambda/LambdaImpl2.java
@@ -2,11 +2,8 @@ package com.bhas.lambda;
 
 import java.time.LocalDate;
 
-public class LambdaImpl2 
-{
-
-	public static void main(String[] args) 
-	{
+public class LambdaImpl2 {
+	public static void main(String[] args) {
 		LambdaEx.m1();
 		LambdaEx l1 = year ->year-LocalDate.now().getYear();
 		int age = l1.getAge(1996);
