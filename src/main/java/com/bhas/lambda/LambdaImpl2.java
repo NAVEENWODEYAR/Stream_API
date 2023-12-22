8250981a58e00f01d3c1eae8b716785b71c55fbc
Naveen <naveenkrishnawodeyar@gmail.com>
2023-12-23 00:00:32 +0530

    InterruptedException

--- src/main/java/com/bhas/lambda/LambdaImpl2.java
+++ src/main/java/com/bhas/lambda/LambdaImpl2.java
@@ -3,7 +3,7 @@ package com.bhas.lambda;
 import java.time.LocalDate;
 
 public class LambdaImpl2 {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		LambdaEx.m1();
 		LambdaEx l1 = year ->year-LocalDate.now().getYear();
 		int age = l1.getAge(1996);
@@ -11,5 +11,6 @@ public class LambdaImpl2 {
 		
 		Thread t = new Thread("T");
 		t.run();
+		t.wait();
 	}
 }
