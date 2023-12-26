faff7a6d3fbf4379da9a5a7fb0904021229177ad
Naveen <naveenkrishnawodeyar@gmail.com>
2023-12-26 23:03:04 +0530

    logOut

--- src/main/java/com/bhas/date/DateFormates.java
+++ src/main/java/com/bhas/date/DateFormates.java
@@ -7,7 +7,7 @@ public class DateFormates {
 	public static void main(String[] args) {
 		Date date = new Date(System.currentTimeMillis());
 		System.out.println(date);
-		System.out.println(date.getDate());
+		System.out.println(date.getYear());
 		System.out.println(DateFormates.class.getCanonicalName().trim().endsWith("d"));
 	}
 }
