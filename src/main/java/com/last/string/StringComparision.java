f3d995786a1f6a4ccd96dd632664e1d9a0bb57e9
Naveen <naveenkrishnawodeyar@gmail.com>
2023-12-31 23:13:16 +0530

    String comparision

---
+++ src/main/java/com/last/string/StringComparision.java
@@ -0,0 +1,14 @@
+package com.last.string;
+
+public class StringComparision {
+
+	public static void main(String[] args) {
+		System.out.println("\nString Comparision,");
+		
+		String st = "String";
+		String s1 = new String("String");
+		
+		System.out.println(st.equals(s1));
+	}
+
+}
