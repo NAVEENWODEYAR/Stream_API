0048226f8f04a66fc9e90d8a8880bd0514c40fc8
Naveen <naveenkrishnawodeyar@gmail.com>
2023-12-31 23:41:01 +0530

    @Override public boolean equals()

--- src/main/java/com/last/string/StringComparision.java
+++ src/main/java/com/last/string/StringComparision.java
@@ -2,13 +2,19 @@ package com.last.string;
 
 public class StringComparision {
 
+	@Override
+	public boolean equals(Object obj) {
+		return true;
+	}
+	
 	public static void main(String[] args) {
 		System.out.println("\nString Comparision,");
 		
-		String st = "String";
-		String s1 = new String("String");
+		String st = "2023";
+		String s1 = "2024";
 		
 		System.out.println(st.equals(s1));
 	}
 
 }
+ 
